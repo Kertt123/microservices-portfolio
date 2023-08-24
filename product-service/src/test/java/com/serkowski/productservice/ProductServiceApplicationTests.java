b82704cdf451e3d7026927edc02c20865f88cdb0
Krzysztof_Serkowski <krzysztof_serkowski@epam.com>
2023-08-24 13:41:53 +0200

    Product service init

---
+++ product-service/src/test/java/com/serkowski/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serkowski.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
