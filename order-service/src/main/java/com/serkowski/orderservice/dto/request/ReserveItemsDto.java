80388ee2572a1d10dbb51b6861eec71ec1b9a87f
Krzysztof_Serkowski <krzysztof_serkowski@epam.com>
2023-08-30 11:34:37 +0200

    web client simple

---
+++ order-service/src/main/java/com/serkowski/orderservice/dto/request/ReserveItemsDto.java
@@ -0,0 +1,18 @@
+package com.serkowski.orderservice.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReserveItemsDto {
+
+    private List<String> ids;
+    private List<String> serialNumbers;
+}
--- order-service/src/main/java/com/serkowski/orderservice/service/impl/OrderServiceImpl.java
+++ order-service/src/main/java/com/serkowski/orderservice/service/impl/OrderServiceImpl.java
@@ -1,6 +1,8 @@
 package com.serkowski.orderservice.service.impl;
 
+import com.serkowski.orderservice.dto.request.OrderItemRequestDto;
 import com.serkowski.orderservice.dto.request.OrderRequest;
+import com.serkowski.orderservice.dto.request.ReserveItemsDto;
 import com.serkowski.orderservice.dto.response.OrderResponse;
 import com.serkowski.orderservice.model.OrderSummary;
 import com.serkowski.orderservice.model.State;
@@ -11,8 +13,11 @@ import com.serkowski.orderservice.service.api.OrderMapper;
 import com.serkowski.orderservice.service.api.OrderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @RequiredArgsConstructor
@@ -23,10 +28,22 @@ public class OrderServiceImpl implements OrderService {
     private final OrderWriteRepository orderWriteRepository;
     private final OrderReadRepository orderReadRepository;
     private final OrderMapper orderMapper;
+    private final WebClient.Builder webClientBuilder;
 
     @Override
     public OrderResponse placeOrderDraft(OrderRequest orderRequest) {
         OrderSummary orderSummary = orderWriteRepository.save(orderMapper.map(orderRequest, State.DRAFT));
+        webClientBuilder.build().post()
+                .uri("http://localhost:8082/api/product/items/reserve")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(ReserveItemsDto.builder()
+                        .ids(orderRequest.getOrderItems().stream().map(OrderItemRequestDto::getItemRef).toList())
+                        .build()))
+                .retrieve()
+                .bodyToMono(String.class)
+                .toFuture()
+                .thenAccept(response -> log.info("Reserve items response: " + response));
 
         return orderMapper.map(orderSummary);
     }
